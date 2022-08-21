05db67d9e3119b0a06918d994840ff3562ffc1cb
Gerardo <gerardo.domra@gmail.com>
2022-08-21 12:00:39 -0500

    Add CORS functions

--- intelligenttutor.py
+++ intelligenttutor.py
@@ -1,10 +1,10 @@
 from concurrent.futures import thread
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
@@ -12,10 +12,10 @@ def index():
     return jsonify(response)
 
 @app.route('/intelligenttutor/api', methods = ['POST'], endpoint = 'api')
+@cross_origin()
 def kinematic():
-    req = req.get_json()
-    print(req)
+    req = request.get_json()
     return jsonify(dict({'response': 'true'}))
 
 if __name__ == '__main__':
-    app.run(threaded = True, port = 5000)
+    app.run(debug = True, threaded = True, port = 5000)
