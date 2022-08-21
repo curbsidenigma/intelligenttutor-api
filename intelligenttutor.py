3e693936bc60a809fa454f442adc1320dfca06e1
Gerardo <gerardo.domra@gmail.com>
2022-08-21 11:46:27 -0500

    Add 'kinematic' route

--- intelligenttutor.py
+++ intelligenttutor.py
@@ -8,7 +8,14 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
-    return jsonify(dict({'app': 'intelligenttutor'}))
+    response = dict({'app': 'intelligenttutor'})
+    return jsonify(response)
+
+@app.route('/intelligenttutor/api', methods = ['POST'], endpoint = 'api')
+def kinematic():
+    req = req.get_json()
+    print(req)
+    return jsonify(dict({'response': 'true'}))
 
 if __name__ == '__main__':
     app.run(threaded = True, port = 5000)
