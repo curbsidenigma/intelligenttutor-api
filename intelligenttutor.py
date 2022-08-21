708706d1df3bf7249defc1666a43c65e101422cf
Gerardo <gerardo.domra@gmail.com>
2022-08-21 12:04:58 -0500

    Create dictionary() class and stringify() function

--- intelligenttutor.py
+++ intelligenttutor.py
@@ -6,16 +6,37 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+class dictionary(dict):
+    def __init__(self):
+        self = dict()
+    def add(self, key, value):
+        self[key] = value
+
+def stringifyArray(array):
+    stringified = []
+    for element in array:
+        stringified.append(str(round(element, 2)))
+    return stringified
+
 @app.route('/')
 def index():
-    response = dict({'app': 'intelligenttutor'})
+    response = dictionary()
+    response.add('app', 'intelligenttutor-api')
     return jsonify(response)
 
 @app.route('/intelligenttutor/api', methods = ['POST'], endpoint = 'api')
 @cross_origin()
 def kinematic():
+    r = []
+
     req = request.get_json()
-    return jsonify(dict({'response': 'true'}))
+    res = dictionary()
+
+    for data in req['r']:
+        r.append(data['magnitude'])
+    res.add('r', stringifyArray(r))
+
+    return jsonify(res), 200
 
 if __name__ == '__main__':
     app.run(debug = True, threaded = True, port = 5000)
