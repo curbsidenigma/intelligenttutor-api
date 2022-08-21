32179bbf5c6aca4d9b3127d1b5a5c11d5f5c938a
Gerardo <gerardo.domra@gmail.com>
2022-08-21 12:17:50 -0500

    Add positiveRad() function and theta solver

--- intelligenttutor.py
+++ intelligenttutor.py
@@ -2,6 +2,9 @@ from concurrent.futures import thread
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
+from sympy import *
+import math
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -18,6 +21,11 @@ def stringifyArray(array):
         stringified.append(str(round(element, 2)))
     return stringified
 
+def positiveRad(angle):
+    if angle < 0:
+        angle = 2*math.pi + angle
+    return angle
+
 @app.route('/')
 def index():
     response = dictionary()
@@ -28,6 +36,7 @@ def index():
 @cross_origin()
 def kinematic():
     r = []
+    th = []
 
     req = request.get_json()
     res = dictionary()
@@ -35,6 +44,25 @@ def kinematic():
     for data in req['r']:
         r.append(data['magnitude'])
     res.add('r', stringifyArray(r))
+
+    for data in req['theta']:
+        th.append(data['magnitude'])
+
+    # Declare symbolic variables
+    x = Symbol('x', real = True)
+    y = Symbol('y', real = True)
+
+    # Solve for angular position
+    f1 = r[2]*cos(x) - r[3]*cos(y) - r[0] + r[1]*cos(th[1])
+    f2 = r[2]*sin(x) - r[3]*sin(y) + r[1]*sin(th[1])
+    sol = solve([f1, f2], x, y)
+    if sol[0][0] > 0:
+        th.append(positiveRad(sol[0][0]))
+        th.append(positiveRad(sol[0][1]))
+    else:
+        th.append(positiveRad(sol[1][0]))
+        th.append(positiveRad(sol[1][1]))
+    res.add('theta', stringifyArray(th))
 
     return jsonify(res), 200
 
